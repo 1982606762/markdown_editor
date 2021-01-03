04101cd0a2b9b85cf5fac13282952d11fbe0689e
zhaoxuanlang <1982606762@qq.com>
2021-01-03 17:00:01 +0800

    1.3

--- app/main.js
+++ app/main.js
@@ -1,71 +1,149 @@
 const { NONAME } = require('dns');
-const { app, BrowserWindow, dialog } = require('electron');
+const { app, BrowserWindow, dialog, remote } = require('electron');
 const fs = require('fs');
-
+const openwindows = new Map();
 const windows = new Set();
 let mainWindow = null;
 
 app.on('ready', () => {
-    createwindow();
+  createwindow();
 });
 
-app.on('window-all-closed',()=>{
-    if(process.platform=='darwin'){
-      return false;
-    }else{
-      app.quit();
-    }
+app.on('window-all-closed', () => {
+  if (process.platform == 'darwin') {
+    return false;
+  } else {
+    app.quit();
+  }
 })
 
-app.on('activate',(event,hasVisibleWindows)=>{
-  if(!hasVisibleWindows) { createwindow();}　
+app.on('activate', (event, hasVisibleWindows) => {
+  if (!hasVisibleWindows) { createwindow(); }
 })
 
+app.on('will-finish-launching', () => {
+  app.on('open-file', (event, file) => {
+    const win = createwindow();
+    win.once('ready-to-show', () => {
+      openFile(win, file);
+    })
+  })
+})
 const getFileFromUser = exports.getFileFromUser = (targetwindow) => {
   dialog.showOpenDialog(targetwindow, {
-      properties: ['openFile'],
-      filters: [
-          { name: 'Text Files', extensions: ['txt'] },
-          { name: 'Markdown Files', extensions: ['md', 'markdown'] }
-      ]
+    properties: ['openFile'],
+    filters: [
+      { name: 'Text Files', extensions: ['txt'] },
+      { name: 'Markdown Files', extensions: ['md', 'markdown'] }
+    ]
   }).then(result => {
-      if (result.filePaths.length > 0) { openFile(targetwindow,result.filePaths[0]); }
+    if (result.filePaths.length > 0) { openFile(targetwindow, result.filePaths[0]); }
   }).catch(err => {
-      console.log(err);
+    console.log(err);
   })
 };
 
-const openFile = (targetwindow,file) => {
+const openFile = exports.openFile =  (targetwindow, file) => {
   const content = fs.readFileSync(file).toString();
-  targetwindow.webContents.send('file-opened', file, content); // B
+  targetwindow.webContents.send('file-opened', file, content);
+  app.addRecentDocument(file);
+  targetwindow.setRepresentedFilename(file);
 };
 
-const createwindow = exports.createwindow = ()=>{
-    let x,y;
-    const currentwindow = BrowserWindow.getFocusedWindow()
-    if(currentwindow){
-      const[currentx,currenty]=currentwindow.getPosition();
-      x=currentx+20;
-      y=currenty+20;
-    }
-    let newwindow = new BrowserWindow({x,y,
-      show:false,
-      webPreferences:{
-        nodeIntegration:true,
-        enableRemoteModule:true
-      }
-    })
-    newwindow.loadFile(`${__dirname}/index.html`);
-    
-    newwindow.once('ready-to-show',()=>{
-        newwindow.show();
-    })
+const savefile = exports.savefile = (targetwindow, content) => {
+  let file=null;  
+  dialog.showSaveDialog(targetwindow, {
+      title: 'save html',
+      defaultPath: app.getPath("documents"),
+      buttonLabel: '保存',
+      filters: [
+        { name: "html file", extensions: ["html"] }
+      ]
+    }).then(result => {
+      file=result.filePath;
+      fs.writeFileSync(result.filePath, content);
+      console.log(result)
+    }).catch(err => {
+      console.log(err)
+    });
+  if (!file) { return; }
+  console.log(file);
+  fs.writeFileSync(file, content);
+}
 
-    newwindow.on('closed',()=>{
-      windows.delete(newwindow);  
-      newwindow=null
+const saveMarkdown = exports.saveMarkdown = (targetWindow, file, content) => {
+
+  if (!file) {
+    dialog.showSaveDialog(targetWindow, {
+      title: 'Save Markdown',
+      defaultPath: app.getPath('documents'),
+      filters: [
+        { name: 'Markdown Files', extensions: ['md', 'markdown'] }
+      ]
     }
-    );
-    windows.add(newwindow);
-    return newwindow;
+    ).then(result => {
+      fs.writeFileSync(result.filePath, content);
+      openFile(targetWindow,result.filePath);
+      console.log(result)
+    }).catch(err => {
+      console.log(err)
+    });
+  }
+  if (!file) return;
+  console.log(file);
+
+  fs.writeFileSync(file, content);
+  openFile(targetWindow, file);
+};
+
+
+
+const createwindow = exports.createwindow = () => {
+  let x, y;
+  const currentwindow = BrowserWindow.getFocusedWindow()
+  if (currentwindow) {
+    const [currentx, currenty] = currentwindow.getPosition();
+    x = currentx + 20;
+    y = currenty + 20;
+  }
+  let newwindow = new BrowserWindow({
+    x, y,
+    show: false,
+    webPreferences: {
+      nodeIntegration: true,
+      enableRemoteModule: true
+    }
+  })
+  newwindow.loadFile(`${__dirname}/index.html`);
+
+  newwindow.once('ready-to-show', () => {
+    newwindow.show();
+  })
+
+  newwindow.on('closed', () => {
+    windows.delete(newwindow);
+    stopwatchingfile(newwindow);
+    newwindow = null
+  }
+  );
+  windows.add(newwindow);
+  return newwindow;
+}
+
+const startwatchingfile = (targetwindow,file)=>{
+  stopwatchingfile(targetwindow);
+  const watch = fs.watchFile(file,(event)=>{
+    if (event=='change') {
+      const content = fs.readFileSync(file);
+      targetwindow.webContents.send('file-opened',file,content);
+    }
+  });
+  openwindows.set(targetwindow,watch);
+}
+
+const stopwatchingfile = (targetWindow)=>{
+  if (openwindows.has(targetWindow)) {
+    openwindows.get(targetWindow).stop();
+    openwindows.delete(targetWindow);
+  }
 }
--- app/renderer.js
+++ app/renderer.js
@@ -2,6 +2,7 @@ const { remote, ipcRenderer } = require('electron');
 const mainProcess = remote.require('./main.js');
 
 const marked = require('marked');
+const path = require('path');
 
 const markdownView = document.querySelector('#markdown');
 const htmlView = document.querySelector('#html');
@@ -15,13 +16,59 @@ const openInDefaultButton = document.querySelector('#open-in-default');
 
 const currentwindow = remote.getCurrentWindow();
 
+let filepath = null;
+let originalcontent = '';
+
+const updatetitle = (isedited)=>{
+  let title = 'markdown_editor';
+  if(filepath){title=`${path.basename(filepath)} - ${title}`};
+  if(isedited){title=`*${title}`;}
+  currentwindow.setTitle(title);
+  currentwindow.setDocumentEdited(isedited);
+  saveMarkdownButton.disabled=!isedited;
+  revertButton.disabled=!isedited;
+}
 const renderMarkdownToHtml = (markdown) => {
   htmlView.innerHTML = marked(markdown);
+};
+
+const getDraggedFile=(event)=>event.dataTransfer.items[0]; 
+const getDroppedFile=(event)=>event.dataTransfer.files[0];
+
+const fileTypeIsSupported =(file) => {
+  return ['text/plain','text/markdown','text/x-markdown'].includes(file.type);
 };
 
 markdownView.addEventListener('keyup', (event) => {
   const currentContent = event.target.value;
   renderMarkdownToHtml(currentContent);
+  updatetitle(currentwindow!=originalcontent);
+});
+
+markdownView.addEventListener('dragover',(event)=>{
+  const file = getDraggedFile(event);
+  console.log(file.type);
+  if(fileTypeIsSupported(file)){
+    markdownView.classList.add('drag-over');
+  }else{
+    markdownView.classList.add('drag-error')
+  }
+});
+
+markdownView.addEventListener('dragleave',()=>{
+  markdownView.classList.remove('drag-over');
+  markdownView.classList.remove('drag-error');
+})
+
+markdownView.addEventListener('drop',(event)=>{
+  const file = getDroppedFile(event);
+  if (fileTypeIsSupported(file)){
+  mainProcess.openFile(currentwindow,file.path);} else {
+  alert('That file type is not supported'); }
+  
+  markdownView.classList.remove('drag-over'); 
+  markdownView.classList.remove('drag-error');
+
 });
 
 openFileButton.addEventListener('click', () => {
@@ -31,7 +78,30 @@ openFileButton.addEventListener('click', () => {
 newFileButton.addEventListener('click',()=>{
   mainProcess.createwindow();
 })
+
+saveHtmlButton.addEventListener('click',()=>{
+  mainProcess.savefile(currentwindow,htmlView.innerHTML);
+})
+
+saveMarkdownButton.addEventListener('click',()=>{
+  mainProcess.saveMarkdown(currentwindow,filepath,markdownView.value);
+})
+
+revertButton.addEventListener('click',()=>{
+  markdownView.value = originalcontent;
+  renderMarkdownToHtml(originalcontent);
+})
+
+document.addEventListener('dragstart',event=>event.preventDefault());
+document.addEventListener('dragover',event=>event.preventDefault());
+document.addEventListener('drop',event=>event.preventDefault());
+document.addEventListener('dragleave',event=>event.preventDefault());
+
 ipcRenderer.on('file-opened', (event, file, content) => {
+  filepath = file;
+  originalcontent = content;
+
   markdownView.value = content;
   renderMarkdownToHtml(content);
+  updatetitle();
 });
